f54f5d66bb55d6a27dffed266ea1e35d37086869
Asphodeluv <seilahaa@gmail.com>
2026-03-03 15:56:17 +0200

    feat(App.java): Implement Consumer, Predicate, and Function interfaces

--- java-project/exercise/App.java
+++ java-project/exercise/App.java
@@ -3,10 +3,11 @@ package exercise;
 /**
  * Exercise.
  */
-public final class App {
+public final class App implements Consumer<String>, Predicate<String>,
+Function<String, String> {
+    private static final int DEF_NUM = 5;
 
-    public static void main(final String[] args) {
-
+    public final class App {
         var names = getNames();
         for (String name : names) {
             System.out.println(name);
@@ -19,5 +20,20 @@ public final class App {
         names.add("tina smith");
         names.add("paul bond");
         return names;
+    }
+     @Override
+    public void accept(final String t) {
+        System.out.print(t + "\n");
+    }
+
+    // return true if you want to keep t in List, otherwise return false
+    @Override
+    public boolean test(final String t) {
+        return t.length() >= DEF_NUM;
+    }
+
+    @Override
+    public String apply(final String t) {
+        return t.toUpperCase();
     }
 }
