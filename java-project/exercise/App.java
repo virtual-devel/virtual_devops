0d9060a310e9193a897e0f7afdb6c9128d65b6e1
Asphodeluv <seilahaa@gmail.com>
2026-03-03 15:51:44 +0200

    feat(App.java): Print names in main method

--- java-project/exercise/App.java
+++ java-project/exercise/App.java
@@ -7,13 +7,17 @@ public final class App {
 
     public static void main(final String[] args) {
 
+        var names = getNames();
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
-}
 
-public static List<String> getNames() {
-    var names = new LinkedList<String>();
-    names.add("jack");
-    names.add("tina smith");
-    names.add("paul bond");
-    return names;
+    public static List<String> getNames() {
+        var names = new LinkedList<String>();
+        names.add("jack");
+        names.add("tina smith");
+        names.add("paul bond");
+        return names;
+    }
 }
