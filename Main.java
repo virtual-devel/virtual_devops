0176109e135efb90b762a41dc5a0dc22c78a607c
virtual-devel <ellen.leino@tuni.fi>
2026-03-03 15:22:57 +0200

    feat(Main.java): add hello world

--- Main.java
+++ Main.java
@@ -2,5 +2,9 @@
  * Javadoc.
  */
 
-public class Main {}
-
+public class Main {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
