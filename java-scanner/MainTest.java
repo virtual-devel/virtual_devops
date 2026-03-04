0bf0336661be0f6d6c5c3bfb8a116e439261a13d
Asphodeluv <seilahaa@gmail.com>
2026-03-04 13:53:20 +0200

    feat(java-scanner/): Add MainTest class with JUnit setup

---
+++ java-scanner/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+}
