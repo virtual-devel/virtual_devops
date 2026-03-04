b047f31e88e637c0cd4f03b64b2c287d7a1b7d42
virtual-devel <ellen.leino@tuni.fi>
2026-03-04 14:35:25 +0200

    feat(HelloWorld): update class

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
  * Say hello to the world.
  */
 
-public class Main {
+class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
