d045dcb9cc8f316c8f6a8794d2715003ab8a4335
Asphodeluv <seilahaa@gmail.com>
2026-03-04 13:36:28 +0200

    feat(Main.java): Add try-catch for error handling

--- java-scanner/Main.java
+++ java-scanner/Main.java
@@ -3,45 +3,49 @@ import java.util.Scanner;
 public class Main {
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Give number 1: ");
+            double num1 = Double.parseDouble(sc.nextLine());
 
-        System.out.print("Give number 1: ");
-        double num1 = Double.parseDouble(sc.nextLine());
+            System.out.print("Give operator (+, -, *, /): ");
+            String operator = sc.nextLine();
 
-        System.out.print("Give operator (+, -, *, /): ");
-        String operator = sc.nextLine();
+            System.out.print("Give number 2: ");
+            double num2 = Double.parseDouble(sc.nextLine());
 
-        System.out.print("Give number 2: ");
-        double num2 = Double.parseDouble(sc.nextLine());
+            double result;
 
-        double result;
-
-        switch(operator) {
-            case "+":
-                result = num1 + num2;
-                System.out.println("Result: " + result);
-                break;
-
-            case "-":
-                result = num1 - num2;
-                System.out.println("Result: " + result);
-                break;
-
-            case "*":
-                result = num1 * num2;
-                System.out.println("Result: " + result);
-                break;
-
-            case "/":
-                if (num2 == 0) {
-                    System.out.println("Error: Division by zero is not allowed.");
-                    break;
-                } else {
-                    result = num1 / num2;
+            switch(operator) {
+                case "+":
+                    result = num1 + num2;
                     System.out.println("Result: " + result);
                     break;
-                }
-            default:
-                System.out.println("Error: Invalid operator.");
+
+                case "-":
+                    result = num1 - num2;
+                    System.out.println("Result: " + result);
+                    break;
+
+                case "*":
+                    result = num1 * num2;
+                    System.out.println("Result: " + result);
+                    break;
+
+                case "/":
+                    if (num2 == 0) {
+                        System.out.println("Error: Division by zero is not allowed.");
+                        break;
+                    } else {
+                        result = num1 / num2;
+                        System.out.println("Result: " + result);
+                        break;
+                    }
+                default:
+                    System.out.println("Error: Invalid operator.");
+            }
+        } catch(Exception e) {
+            System.out.println("Something went wrong.");
+            return;
         }
     }
 }
